4a669edc5c6475f0449ceb144e3aea72aa5373b7
Max Bauer <maxbauer@Mac.zyxel.com>
2025-07-05 02:00:02 -0400

    added exanded with images. still need to fix image adjusting width

--- src/components/ExpandableDiv.jsx
+++ src/components/ExpandableDiv.jsx
@@ -1,25 +1,34 @@
-import React, {useEffect, useState, useRef} from 'react';
+import React, {useEffect, useState} from 'react';
+import Photo from './PropPractice';
 
-function ExpandableDiv({isActive, onChildClick}) {
-
+function ExpandableDiv({label, imgId}) {
+  const [expanded, setExpanded] = useState(false);
   const [hovered, setHovered] = useState(false);
-  
+
+  const toggleExpand = () => setExpanded(prev => !prev);
   const mouseEnter = () => setHovered(true);
   const mouseLeave = () => setHovered(false);
 
+
   return (
     <div 
-      onClick={onChildClick}
+      onClick={toggleExpand}
       onMouseEnter={mouseEnter}
       onMouseLeave={mouseLeave}
-      className={`transition-all duration-600 overflow-hidden cursor-pointer bg-neutral-400 rounded p-4
-        h-200 ${isActive ? 'w-108' : 'w-24'} ${isActive ? 'hover:none' : 'hover:bg-blue-200'}`
+      className={`transition-[width, colors, text] duration-600 overflow-hidden cursor-pointer bg-neutral-200 rounded p-2
+        h-200 hover:border-2 hover:border-black shadow-md
+        ${expanded ? 'w-308' : 'w-24'} ${expanded ? 'hover:none' : 'hover:bg-blue-200'}
+        ${expanded ? 'flex flex-col' : 'relative overflow-hidden'}
+        `
 
       }
     >
-       <p className="font-bold">Click me to {expanded ? 'collapse' : 'expand'}!</p>
-       {isActive && <p>This content is revealed when expanded.</p>}
-       {hovered && <p> This box is being hovered</p>}
+      <div class="h-1/2 overflow-hidden absolute "><Photo imgId={imgId}></Photo></div>
+      {!expanded && <p className="font-bold [writing-mode:vertical-lr] rotate-180 text-6xl">{label}</p>}
+      {/* {expanded && <div class="h-1/2 w-full overflow-hidden "><Photo imgId={imgId}></Photo></div>} */}
+      
+      {expanded && <div class="h-1/2 w-full">asdfasdfasdfa</div>} 
+      {/* {hovered && <p> This box is being hovered</p>} */}
       
     </div>
   )
--- src/components/PropPractice.jsx
+++ src/components/PropPractice.jsx
@@ -3,16 +3,16 @@ import { getImageUrl } from "./utils";
 function Landscape({image, size}) {
   return (
 		<img
-			className="landscape"
+			class="h-200 w-400 mr-4"
 			src={getImageUrl(image)}
 			alt={image.name}
-			width={size}
-			height={size}
+			// width={size}
+			// height={size}
 		/>
 	);
   }
 
-export default function Photo(){
+export default function Photo({imgId}){
 
 	const imageIds = ['edUW7cJ', 'jRetXxS', 'KvSDVHD', 'zs7lXPL', 'rfig4mQ', 'GdLn4KT']
 
@@ -22,7 +22,7 @@ export default function Photo(){
 		<div>
 			<Landscape
 				size={"100%"}
-				image={{ name: 'aPhoto', imageId: randomValue }}
+				image={{ name: 'aPhoto', imageId: imgId }}
 			/>  
 		</div>                  
 	)
