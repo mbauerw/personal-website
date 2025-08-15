8558e93b2e5d293e603ae45219a8cac755fb0888
maxbauerw@gmail.com <maxbauerw@gmail.com>
2025-08-15 00:09:40 -0400

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,9 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss()],
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}', // Make sure this includes your SkillCategory files
+      './components/**/*.{js,jsx,ts,tsx}',
+    ],
+    // ...
 })
-
-module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Make sure this includes your SkillCategory files
-    './components/**/*.{js,jsx,ts,tsx}',
-  ],
-  // ...
-}
